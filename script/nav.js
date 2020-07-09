6eb7f45f8e008a43730f846ca6df517182dfd2f5
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-09 16:27:35 +0100

    Update nav.js

--- script/nav.js
+++ script/nav.js
@@ -1,1 +1,12 @@
-*
+(function () {
+  
+  function goTo ($ele) {
+    let main = document.body.querySelector("main");
+    main.parentNode.replaceChild($ele, main);
+  };
+  
+  $.nav = {
+    "goTo": goTo
+  };
+  
+}());
