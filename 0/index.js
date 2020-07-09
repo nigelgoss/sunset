a5c04b953671f08e2b5bb01539a0001ab7ed8932
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-09 16:36:54 +0100

    Update index.js

--- 0/index.js
+++ 0/index.js
@@ -9,11 +9,13 @@ const $ = {};
   ].forEach(function ($v) {
 
     let script = document.createElement("script"); document.head.appendChild(script);
+    script.defer = true;
     script.src = $v;
     
   });
   
   let script = document.createElement("script"); document.head.appendChild(script);
+  script.defer = true;
   script.textContent = "(function () { $.nav.goTo('Sign In'); }());";
   
 }());
