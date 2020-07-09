eac70986a9cf87ff2222b64bc856b0da58408d00
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-09 16:57:31 +0100

    Update patient_list.js

--- pages/patient_list.js
+++ pages/patient_list.js
@@ -1,1 +1,20 @@
-*
+(function () {
+  
+  function load ($d, $cb) {
+    $cb(main);
+  };
+  
+  function back ($d, $cb) {
+    $cb(main);
+  };
+  
+  let main = document.createElement("main");
+  main.textContent = "Sign In";
+  
+  $.pages["Patient List"] = {
+    "forwards": load,
+    "backwards": back,
+  };
+  
+  
+}());
