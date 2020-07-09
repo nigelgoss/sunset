670f1509321be1e27569a75666b5785c1a7bfb19
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-09 16:18:37 +0100

    Update index.js

--- 0/index.js
+++ 0/index.js
@@ -1,7 +1,11 @@
 (function () {
   
+  const $ = {};
+  $.pages = {};
+  
   [
-  "pages/sign_in.js"
+  "pages/sign_in.js",
+  "script/nav.js",
   ].forEach(function ($v) {
 
     let script = document.createElement("script"); document.head.appendChild(script);
