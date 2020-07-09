b3b06bec77939d374891dc53f8b92b191b955f4e
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-09 16:17:17 +0100

    Update sign_in.js

--- pages/sign_in.js
+++ pages/sign_in.js
@@ -1,1 +1,18 @@
-*
+(function () {
+  
+  function load () {
+    $.nav.goTo(main);
+  };
+  
+  let main = document.createElement("main");
+  
+  let button = document.createElement("button"); main.appendChild(button);
+  button.textContent = "Sign In";
+  
+  $.pages["Sign In"] = {
+    "forwards": load,
+    "backwards": back,
+  };
+  
+  
+}());
