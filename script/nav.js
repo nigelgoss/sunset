d2dd7ebbd7132c5c321b324fbe54298290e9ec0c
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-10 11:45:00 +0100

    Update nav.js

--- script/nav.js
+++ script/nav.js
@@ -7,13 +7,19 @@
   function updateViewport ($d) {
     let main = document.body.querySelector("main");
     main.parentNode.replaceChild($d.ele, main);
-    document.body.querySelector("header").textContent = $d.title;
+    title.textContent = $d.title;
   };
   
   let header = document.createElement("header");
   header.style.backgroundColor = "lightblue";
   header.style.padding = "10px";
   header.style.textAlign = "center";
+  
+  const title = document.createElement("span"); header.appendChild(title);
+  
+  let button = document.createElement("button"); header.appendChild(button);
+  button.textContent = "Admin";
+  button.onpointerdown = function () { $.nav.goTo("Admin"); };
   
   let oldHeader = document.body.querySelector("header");
   oldHeader.parentNode.replaceChild(header, oldHeader);
