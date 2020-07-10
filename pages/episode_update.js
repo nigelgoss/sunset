d2cf1e1ed1941fee3a8bdaec6e46734321dfaf9c
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-10 11:40:53 +0100

    Update episode_update.js

--- pages/episode_update.js
+++ pages/episode_update.js
@@ -1,1 +1,21 @@
-*
+(function () {
+  
+  function load ($d, $cb) {
+    $cb(main);
+  };
+  
+  function back ($d, $cb) {
+    $cb(main);
+  };
+  
+  let main = document.createElement("main");
+  main.textContent = "*";
+  
+  $.pages["Episode Update"] = {
+    "forwards": load,
+    "backwards": back,
+  };
+  
+  
+}());
+
