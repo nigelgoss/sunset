7bf809a56a5c03586bbb364467579abc6ed6bc1d
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 20:19:58 +0100

    Update episode_update.js

--- pages/episode_update.js
+++ pages/episode_update.js
@@ -1,21 +1,19 @@
-(function () {
-  
-  function load ($d, $cb) {
-     main.textContent = JSON.stringify($d);
-    $cb(main);
-  };
-  
-  function back ($d, $cb) {
-    $cb(main);
-  };
-  
-  const main = document.createElement("main");
-  
-  $.pages["Episode Update"] = {
-    "forwards": load,
-    "backwards": back,
-  };
-  
-  
-}());
+(() => {
 
+const load ($d, $cb) => {
+	main.textContent = JSON.stringify($d);
+	$cb(main);
+};
+
+const back ($d, $cb) => {
+	$cb(main);
+};
+
+const main = document.createElement("main");
+
+$.pages["Episode Update"] = {
+	"forwards": load,
+	"backwards": back,
+};
+
+})();
