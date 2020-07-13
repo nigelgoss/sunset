b8c210256a4e44f34b0a1fe1ebd4d39a01a4c285
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 21:33:57 +0100

    Update patient_list.js

--- pages/patient_list.js
+++ pages/patient_list.js
@@ -65,7 +65,9 @@ const build = () => {
 
 		let tr = document.createElement("tr"); tbody.appendChild(tr);
 
-		let td = document.createElement("td"); tr.appendChild(td); td.textContent = $v.Location;
+		let td = document.createElement("td"); tr.appendChild(td);
+		td.rowSpan = "3";
+		td.textContent = $v.Location;
 		td = document.createElement("td"); tr.appendChild(td); td.textContent = $v.Name;
 		td = document.createElement("td"); tr.appendChild(td); td.textContent = $v.NHSNo;
 		td = document.createElement("td"); tr.appendChild(td); td.textContent = $v.HospitalNo;
@@ -73,7 +75,10 @@ const build = () => {
 		td = document.createElement("td"); tr.appendChild(td); td.textContent = $v.Death;
 
 		td = document.createElement("td"); tr.appendChild(td);
+		td.rowSpan = "2";
+		td.ngstyle = { textAlign: "right", };
 		button = document.createElement("button"); td.appendChild(button);
+		button.ngstyle = { backgroundColor:"#007f3b", color:"#ffffff", borderRadius:"999px", };
 		button.ngpointerdown = () => { $.nav.goTo("Episode Update", {"EPN":$v.EPN, "Episode":$v.Episode}); };
 			let span = document.createElement("span"); button.appendChild(span);
 			span.className = "faS";
@@ -89,115 +94,3 @@ return {
 };
 
 })();
-
-
-/*
-let button = document.createElement("button"); main.appendChild(button);
-button.textContent = "Episode";
-button.onpointerdown = function () {
-	$.nav.goTo("Episode Update", {"EPN":1, "Episode":1});
-};
-
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="3">THH-1</td>
-			<td>SMITH, John (Mr)</td>
-			<td>000-000-0000</td>
-			<td>00000000</td>
-			<td>01 Jan 1900</td>
-			<td>01 Jan 2020</td>
-			<td rowspan="2" style="text-align:right;"><button style="background-color:#007f3b; color:#ffffff; border-radius:999px;" onclick="swap();"><span class="faS"></span></button></td>
-		</tr>
-	</tbody>
-	
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="2">THH-2</td>
-			<td>SMITH, John (Mr)</td>
-			<td>000-000-0000</td>
-			<td>00000000</td>
-			<td>01 Jan 1900</td>
-			<td>01 Jan 2020</td>
-			<td rowspan="2" style="text-align:right;"><button style="background-color:#007f3b; color:#ffffff; border-radius:999px;" onclick="swap();"><span class="faS"></span></button></td>
-		</tr><tr>
-			<td colspan="5"><em>Notes: </em>Something...</td>
-		</tr>
-	</tbody>
-	
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="2">THH-3</td>
-			<td>SMITH, John (Mr)</td>
-			<td>000-000-0000</td>
-			<td>00000000</td>
-			<td>01 Jan 1900</td>
-			<td>01 Jan 2020</td>
-			<td rowspan="2" style="text-align:right;"><button style="background-color:#007f3b; color:#ffffff; border-radius:999px;" onclick="swap();"><span class="faS"></span></button></td>
-		</tr><tr>
-			<td colspan="5"><em>Notes: </em>Something...</td>
-		</tr>
-	</tbody>
-	
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="2">THH-4</td>
-			<td>&nbsp;</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td rowspan="2" style="text-align:right;"></td>
-		</tr>
-	</tbody>
-	
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="2">THH-5</td>
-			<td>SMITH, John (Mr)</td>
-			<td>000-000-0000</td>
-			<td>00000000</td>
-			<td>01 Jan 1900</td>
-			<td>01 Jan 2020</td>
-			<td rowspan="2" style="text-align:right;"><button style="background-color:#007f3b; color:#ffffff; border-radius:999px;" onclick="swap();"><span class="faS"></span></button></td>
-		</tr><tr>
-			<td colspan="5"><em>Notes: </em>Something...</td>
-		</tr>
-	</tbody>
-	
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="2">THH-6-BAR</td>
-			<td>&nbsp;</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td rowspan="2" style="text-align:right;"></td>
-		</tr>
-	</tbody>
-	
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="2">THH-7</td>
-			<td>&nbsp;</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td rowspan="2" style="text-align:right;"></td>
-		</tr>
-	</tbody>
-	
-	<tbody style="border:0 solid white; border-width:2px 0 2px 0;">
-		<tr>
-			<td rowspan="2">THH-8</td>
-			<td>&nbsp;</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td rowspan="2" style="text-align:right;"></td>
-		</tr>
-	</tbody>
-	
-*/
