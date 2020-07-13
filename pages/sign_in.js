36d413727b864418b2c6334d48d9a6b5296d8f47
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 21:03:09 +0100

    Update sign_in.js

--- pages/sign_in.js
+++ pages/sign_in.js
@@ -11,8 +11,8 @@ const back = ($d, $cb) => {
 const main = document.createElement("main");
 main.ngstyle = {
 	flex: "1 1 auto",
-	"display": "grid",
-	"place-items": "center",
+	display: "grid",
+	place-items: "center",
 };
 
 let button = document.createElement("button"); main.appendChild(button);
