367827e410659d6c9e500436f4342126ad85170d
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 21:43:46 +0100

    Update patient_list.js

--- pages/patient_list.js
+++ pages/patient_list.js
@@ -53,7 +53,18 @@ button.ngpointerdown = () => {};
 
 const build = () => {
 
-	const $d = [{}, {}, {}, {}, {}];
+	const $d = [
+		{ "EPN":"1", "Episode":"1", "Location":"THH-1", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"2", "Episode":"1", "Location":"THH-2", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"3", "Episode":"1", "Location":"THH-3", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"4", "Episode":"1", "Location":"THH-4", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"5", "Episode":"1", "Location":"THH-5", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"6", "Episode":"1", "Location":"THH-6", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"7", "Episode":"1", "Location":"THH-7", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"8", "Episode":"1", "Location":"THH-8", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"9", "Episode":"1", "Location":"THH-9", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+		{ "EPN":"10", "Episode":"1", "Location":"THH-10", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000"},
+	];
 	
 	table.querySelectorAll("tbody").forEach(($v) => { $v.parentElement.removeChild($v); });
 	
