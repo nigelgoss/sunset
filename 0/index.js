a22c0e929f8d3f805d6449c0ea61ab9f6d98598d
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 20:06:40 +0100

    Update index.js

--- 0/index.js
+++ 0/index.js
@@ -1,55 +1,51 @@
 const $ = {};
-(function () {
+(() => {
 
-	let throttle = { "date": new Date(0), "ele": null };
-	Object.defineProperties(HTMLElement.prototype, {
-		
-		"ngpointerdown": {
-			"set": function ($d) {
-				if (this._ngX === undefined) this._ngX = {}; 
-				if (this.onpointerdown === null) this.onpointerdown = () => {
-					if (throttle.ele === this && new Date() - throttle.date < (this.ngthrottle ?? 5000)) return;
-					throttle = {"date": new Date(), "ele": this };
-					this._ngX.ngpointerdown();
-				};
-				if (this.ontouchstart === null) this.ontouchstart = () => {}; // iOS
-				this._ngX.ngpointerdown = $d;
-			},
-			"get": function () {
-				return this?._ngX?.ngpointerdown;
-			},
-		},
-					
-		"ngthrottle": {
-			"set": function ($d) {
-				if (this._ngX === undefined) this._ngX = {}; 
-				this._ngX.ngthrottle = $d;
-			},	
-			"get": function () {
-				return this?._ngX?.ngthrottle;
-			},
-		},
-		
-	});
-  
-  $.pages = {};
-  
-  [
-    "pages/sign_in.js",
-    "script/nav.js",
-    "pages/patient_list.js",
-    "pages/episode_update.js",
-    "pages/admin.js",
-  ].forEach(function ($v) {
+const throttle = {};
+Object.defineProperties(HTMLElement.prototype, {
 
-    let script = document.createElement("script"); document.head.appendChild(script);
-    //script.defer = true;
-    script.src = $v;
-    
-  });
+	"ngpointerdown": {
+		"set": function ($d) {
+			if (this._ngX === undefined) this._ngX = {}; 
+			if (this.onpointerdown === null) this.onpointerdown = () => {
+				if (throttle.ele === this && new Date() - throttle.date < (this.ngthrottle ?? 5000)) return;
+				throttle.date = new Date();
+				throttle.ele = this;
+				this._ngX.pointerdown();
+			};
+			if (this.ontouchstart === null) this.ontouchstart = () => {}; // iOS
+			this._ngX.pointerdown = $d;
+		},
+		"get": function () {
+			return this?._ngX?.pointerdown;
+		},
+	},
+
+	"ngthrottle": {
+		"set": function ($d) {
+			if (this._ngX === undefined) this._ngX = {}; 
+			this._ngX.throttle = $d;
+		},	
+		"get": function () {
+			return this?._ngX?.throttle;
+		},
+	},
+
+});
+
+$.pages = {};
+
+[
+	"pages/sign_in.js",
+	"script/nav.js",
+	"pages/patient_list.js",
+	"pages/episode_update.js",
+	"pages/admin.js",
+].forEach(($v) => {
+	let script = document.createElement("script"); document.head.appendChild(script);
+	script.src = $v;
+});
   
-  window.onload = function () {
-    $.nav.goTo("Sign In");
-};
-  
+window.onload = () => { $.nav.goTo("Sign In"); };
+
 }());
