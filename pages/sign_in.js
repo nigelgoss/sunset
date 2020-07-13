5998862dc6acfab35db050697fab0d0c100d6777
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 20:38:32 +0100

    Update sign_in.js

--- pages/sign_in.js
+++ pages/sign_in.js
@@ -1,31 +1,22 @@
-(function () {
-  
-  function load ($d, $cb) {
-    $cb(main);
-  };
-  
-  function back ($d, $cb) {
-    $cb(main);
-  };
-  
-  let main = document.createElement("main");
-  
-  let button = document.createElement("button"); main.appendChild(button);
-  button.textContent = "Sign In";
-  button.ngpointerdown = function () { $.nav.goTo("Patient List"); };
-  
-  let button1 = document.createElement("button"); main.appendChild(button1);
-  button1.textContent = "Count";
-  button1.ngpointerdown = function () { console.log(1); };
-  
-  let button2 = document.createElement("button"); main.appendChild(button2);
-  button2.textContent = "Count";
-  button2.ngpointerdown = function () { console.log(2); };
-  
-  $.pages["Sign In"] = {
-    "forwards": load,
-    "backwards": back,
-  };
-  
-  
-}());
+$.pages["Sign In"] = (() => {
+
+const load = ($d, $cb) => {
+	$cb(main);
+};
+
+const back = ($d, $cb) => {
+	$cb(main);
+};
+
+const main = document.createElement("main");
+
+let button = document.createElement("button"); main.appendChild(button);
+button.textContent = "Sign In";
+button.ngpointerdown = function () { $.nav.goTo("Patient List"); };
+
+return {
+	"forwards": load,
+	"backwards": back,
+};
+
+})();
