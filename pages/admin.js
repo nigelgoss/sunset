923a65ddaf3d390a4b8365d827fe7e08a797dbb9
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 20:18:24 +0100

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,20 +1,19 @@
-(function () {
+(() => {
+
+function load ($d, $cb) {
+	$cb(main);
+};
+
+function back ($d, $cb) {
+	$cb(main);
+};
+
+const main = document.createElement("main");
+main.textContent = "*";
+
+$.pages["Admin"] = {
+	"forwards": load,
+	"backwards": back,
+};
   
-  function load ($d, $cb) {
-    $cb(main);
-  };
-  
-  function back ($d, $cb) {
-    $cb(main);
-  };
-  
-  let main = document.createElement("main");
-  main.textContent = "*";
-  
-  $.pages["Admin"] = {
-    "forwards": load,
-    "backwards": back,
-  };
-  
-  
-}());
+})();
