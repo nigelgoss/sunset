2c70fae1360b742da70fd00770a61e393a52c3cf
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-13 17:35:22 +0100

    Update sign_in.js

--- pages/sign_in.js
+++ pages/sign_in.js
@@ -14,13 +14,13 @@
   button.textContent = "Sign In";
   button.ngpointerdown = function () { $.nav.goTo("Patient List"); };
   
-  button = document.createElement("button"); main.appendChild(button);
-  button.textContent = "Count";
-  button.ngpointerdown = function () { console.log(1); };
+  let button1 = document.createElement("button"); main.appendChild(button1);
+  button1.textContent = "Count";
+  button1.ngpointerdown = function () { console.log(1); };
   
-   button = document.createElement("button"); main.appendChild(button);
-  button.textContent = "Count";
-  button.ngpointerdown = function () { console.log(2); };
+  let button2 = document.createElement("button"); main.appendChild(button2);
+  button2.textContent = "Count";
+  button2.ngpointerdown = function () { console.log(2); };
   
   $.pages["Sign In"] = {
     "forwards": load,
