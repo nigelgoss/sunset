ca01746c2aadb4c17186a87bb5f381cd59590250
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-14 14:33:18 +0100

    Update ngx.js

--- script/ngx.js
+++ script/ngx.js
@@ -8,9 +8,21 @@ const alert = ($msg) => {
 	window.alert($msg);
 };
 
+const ngserialise = ($ele = document.body) => {
+	const d = {};
+	$ele.querySelectorAll("*[name]").forEach(($v) => {
+		if (d[$v.name] === undefined) d[$v.name] = ($v.type === "checkbox") ? [] : null;
+		if ($v.checked === false) return;
+		let value = ($v.value === "") ? null : $v.value;
+		if (Array.isArray(d[$v.name]) === true) { d[$v.name].push(value); } else { d[$v.name] = value; };
+	});
+	return d;
+};
+
 return {
 	"open": open,
 	"alert": alert,
+	"ngserialise": ngserialise,
 };
   
 })();
