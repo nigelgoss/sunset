caee25449a9bf4bebf231b4f7176a47c35ffaad0
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-15 15:55:47 +0100

    Update db.js

--- script/db.js
+++ script/db.js
@@ -1,1 +1,20 @@
+"use strict";
+$.db = (() => {
 
+const query = ($qry, $params = {}, $success) => {
+
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState !== 4) return;
+		if (xhr.status === 200) $success(JSON.parse(xhr.responseText));
+	};
+	xhr.open("POST", "../!/"+$qry+".sql", true);
+	xhr.send(JSON.stringify([$qry, $params]));
+	
+};
+
+return {
+	query: query,
+};
+  
+})();
