61735b2fe84c7423b23139e7fb4f2915fed316f1
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-15 15:59:22 +0100

    Update patient_list.js

--- pages/patient_list.js
+++ pages/patient_list.js
@@ -1,7 +1,7 @@
 "use strict";
 $.pages["Patient List"] = (() => {
 
-const load = ($d, $cb) => {
+const load = ($in, $cb) => {
 	
 	$.db.query("patient_list", {}, ($d) = {
 		build($d);
@@ -10,7 +10,7 @@ const load = ($d, $cb) => {
 	
 };
 
-const back = ($d, $cb) => {
+const back = ($in, $cb) => {
 	$cb(main);
 };
 
@@ -76,21 +76,8 @@ td.ngstyle = { width:"1px", textAlign:"right", };
 	button.textContent = "";
 	button.ngpointerdown = () => {};
 
-const build = () => {
+const build = ($d) => {
 
-	const $d = [
-		{ "EPN":"1", "Episode":"1", "Location":"THH-1", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"2", "Episode":"1", "Location":"THH-2", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"3", "Episode":"1", "Location":"THH-3", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"4", "Episode":"1", "Location":"THH-4", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"5", "Episode":"1", "Location":"THH-5", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"6", "Episode":"1", "Location":"THH-6", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":"Test"},
-		{ "EPN":"7", "Episode":"1", "Location":"THH-7", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"8", "Episode":"1", "Location":"THH-8", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"9", "Episode":"1", "Location":"THH-9", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-		{ "EPN":"10", "Episode":"1", "Location":"THH-10", "Name":"SMITH, John (Mr)", "NHSNo":"000 000 0000", "HospitalNo":"00000000", "Birth":"01 Jan 1900", "Death":"01 Jan 2000", "Notes":null},
-	];
-	
 	table.querySelectorAll("tbody").forEach(($v) => { $v.parentElement.removeChild($v); });
 	
 	$d.forEach(($v) => {
