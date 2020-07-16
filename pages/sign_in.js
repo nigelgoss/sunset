42e4f66839ee3fab04cfb2ebd6b8bc0ca4ea7d85
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-16 14:52:14 +0100

    Update sign_in.js

--- pages/sign_in.js
+++ pages/sign_in.js
@@ -13,7 +13,7 @@ const main = document.createElement("main");
 main.ngstyle = { flex:"1 1 auto", display:"grid", placeItems:"center", background:"url('resources/splash.jpg') no-repeat center center fixed", backgroundSize:"cover" };
 
 let div = document.createElement("div"); main.appendChild(div);
-div.ngstyle = { display:"grid", backgroundColor:"rgb(255,255,255,0.67)", padding:"20px", borderRadius:"10px" };
+div.ngstyle = { display:"grid", backgroundColor:"rgb(255,255,255,0.67)", padding:"20px", borderRadius:"10px", gridTemplateColumns:"auto auto", gridGap:"5px" };
 
 let label = document.createElement("label"); div.appendChild(label); label.textContent = "Username";
 let input = document.createElement("input"); div.appendChild(input); input.type="text";
@@ -22,6 +22,7 @@ label = document.createElement("label"); div.appendChild(label); label.textConte
 input = document.createElement("input"); div.appendChild(input); input.type="password";
 
 let button = document.createElement("button"); div.appendChild(button);
+button.ngstyle = { gridColumn:"1/-1" };
 button.textContent = "Sign In";
 button.ngpointerdown = () => { $.nav.goTo("Patient List"); };
 
