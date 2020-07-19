fb23c27d9108585d4570abd2bdd4f010d27a8369
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-19 22:52:44 +0100

    Update menu.js

--- pages/menu.js
+++ pages/menu.js
@@ -10,7 +10,30 @@ const back = ($d, $cb) => {
 };
 
 const main = document.createElement("main");
-main.textContent = "** Menu **";
+
+let button = document.createElement("button"); main.appendChild(button);
+button.ngstyle = {"border-radius":"999px"};
+button.className = "faS";
+button.textContent = " Patient List";
+button.ngpointerdown = () => { $.nav.back("Patient List"); };
+
+button = document.createElement("button"); main.appendChild(button);
+button.ngstyle = {"border-radius":"999px"};
+button.className = "faS";
+button.textContent = " Admin"; 
+button.ngpointerdown = () => { $.nav.goTo("Admin"); };
+
+button = document.createElement("button"); main.appendChild(button);
+button.ngstyle = {"border-radius":"999px"};
+button.className = "faS";
+button.textContent = " Help";
+button.ngpointerdown = () => { $.ngX.open("https://www.google.com/"); };
+
+button = document.createElement("button"); main.appendChild(button);
+button.ngstyle = {"border-radius":"999px"};
+button.className = "faS";
+button.textContent = " Sign Out";
+button.ngpointerdown = () => { location.reload(true); };
 
 return {
 	forward: forward,
