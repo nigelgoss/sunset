c0632063ca2e71f0641abcdab16efc7c596a56c8
nigelgoss <44325963+nigelgoss@users.noreply.github.com>
2020-07-20 14:37:56 +0100

    Update db.js

--- script/db.js
+++ script/db.js
@@ -30,8 +30,8 @@ const spinner = ($status) => {
 
 const spinnerDiv = document.createElement("div");
 spinnerDiv.ngstyle = {"background-color":"grey", "position":"absolute", "top":"0", "left":"0", "width":"100%", "height":"100%", "place-items":"center", "display":"grid"};
-let span = document.createElement("span"); spinnerDiv.appendChild(span);
-span.textContent = "Please wait...";
+let img = document.createElement("img"); spinnerDiv.appendChild(img);
+img.src = "resources/spinner.png";
 
 return {
 	query: query,
